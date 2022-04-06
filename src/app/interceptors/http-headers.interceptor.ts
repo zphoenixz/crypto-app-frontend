d3559655ad1b159396e1e8ce3aa7b9597d45b6e4
Ramiro Valdez <ramiro.valdez@unosquare.com>
2022-04-06 18:41:57 -0400

    token refreshing handle

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -1,21 +1,77 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError } from "rxjs";
+import { LoginResponseI } from "../models/loginResponse.interface";
+import { ApiService } from "../services/api/api.service";
 
 @Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
-    constructor() { }
+    private isRefreshing = false;
+    private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+    constructor(
+        private apiService: ApiService,
+        private toastrSvc: ToastrService,
+        private router: Router,
+    ) { }
 
     intercept(
         req: HttpRequest<any>,
-        next: HttpHandler
+        next: HttpHandler,
     ): Observable<HttpEvent<any>> {
-        req = req.clone({
+
+        let accessToken = localStorage.getItem('accessToken');
+        if (accessToken != null) {
+            req = this.setAccessToken(req, accessToken);
+        }
+
+        return next.handle(req).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status == 403) {
+                return this.handele403Error(req, next)
+            } else {
+                this.forceLogOut()
+                return throwError(error)
+            }
+        }));
+    }
+
+    setAccessToken(req: HttpRequest<any>, accessToken: string) {
+        return req.clone({
             setHeaders: {
                 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
                 'Access-Control-Allow-Origin': '*'
             }
         });
-        return next.handle(req);
+    }
+
+    handele403Error(req: HttpRequest<any>, next: HttpHandler) {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+            this.refreshTokenSubject.next(null);
+
+            return this.apiService.refreshToken().pipe(
+                switchMap((refreshResponse: LoginResponseI) => {
+                    this.isRefreshing = false;
+                    this.refreshTokenSubject.next(refreshResponse.accessToken);
+                    this.toastrSvc.success("Tokens Refreshed!");
+                    return next.handle(this.setAccessToken(req, refreshResponse.accessToken));
+                }));
+
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter(refreshResponse => refreshResponse != null),
+                take(1),
+                switchMap(accessToken => {
+                    return next.handle(this.setAccessToken(req, accessToken));
+                }));
+        }
+    }
+
+    forceLogOut() {
+        this.toastrSvc.error("Expired Session!");
+        localStorage.clear();
+        this.router.navigate(['login']);
     }
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { LoginRequestI } from 'src/app/models/loginRequest.interface';
 import { LoginResponseI } from 'src/app/models/loginResponse.interface';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { UserDataResponseI } from 'src/app/models/userDataResponse.interface';
 import { UserRegisterRequestI } from 'src/app/models/userRegisterRequest.interface';
 import { UserRegisterResponseI } from 'src/app/models/userRegisterResponse.interface';
@@ -14,53 +14,66 @@ import { UserInfoRequestI } from 'src/app/models/userInfoRequest.interface';
   providedIn: 'root'
 })
 export class ApiService {
-  url: string = "http://ec2-13-57-255-162.us-west-1.compute.amazonaws.com:8084";
+  apiUrl: string = "http://ec2-13-57-255-162.us-west-1.compute.amazonaws.com:8084";
 
   constructor(private http: HttpClient) { }
 
   login(form: LoginRequestI): Observable<LoginResponseI> {
-    let loginEndPoint = `${this.url}/user-auth-service/user/auth/login`;
+    let loginEndPoint = `${this.apiUrl}/user-auth-service/user/auth/login`;
 
     return this.http.post<LoginResponseI>(loginEndPoint, form);
   }
 
   getUserByEmail(email: string): Observable<UserDataResponseI> {
-    let getUserByEmailEndPoint = `${this.url}/user-registration-service/user/registration/getUserByEmail?email=${email}`;
+    let getUserByEmailEndPoint = `${this.apiUrl}/user-registration-service/user/registration/getUserByEmail?email=${email}`;
     return this.http.get<UserDataResponseI>(getUserByEmailEndPoint).pipe(
       map(response => response)
     );
   }
 
   saveUser(form: UserRegisterRequestI): Observable<UserRegisterResponseI> {
-    let loginEndPoint = `${this.url}/user-registration-service/user/registration/saveUser`;
+    let loginEndPoint = `${this.apiUrl}/user-registration-service/user/registration/saveUser`;
 
     return this.http.post<UserRegisterResponseI>(loginEndPoint, form);
   }
 
   getOperationsForTheLast7DaysByUserEmail(email: string): Observable<OperationResponseI[]> {
-    let getLast7DaysOperations = `${this.url}/user-operation-service/user/operation/getOperationsForTheLast7DaysByUserEmail?email=${email}`;
+    let getLast7DaysOperations = `${this.apiUrl}/user-operation-service/user/operation/getOperationsForTheLast7DaysByUserEmail?email=${email}`;
     return this.http.get<OperationResponseI[]>(getLast7DaysOperations).pipe(
       map(response => response)
     );
   }
 
   getOperationsForTheLast24HoursByUserEmail(email: string): Observable<OperationResponseI[]> {
-    let getLast24HrsOperations = `${this.url}/user-operation-service/user/operation/getOperationsForTheLast24HoursByUserEmail?email=${email}`;
+    let getLast24HrsOperations = `${this.apiUrl}/user-operation-service/user/operation/getOperationsForTheLast24HoursByUserEmail?email=${email}`;
     return this.http.get<OperationResponseI[]>(getLast24HrsOperations).pipe(
       map(response => response)
     );
   }
 
   getOperationsForTheLast30DaysByUserEmail(email: string): Observable<OperationResponseI[]> {
-    let getLast30DaysOperations = `${this.url}/user-operation-service/user/operation/getOperationsForTheLast30DaysByUserEmail?email=${email}`;
+    let getLast30DaysOperations = `${this.apiUrl}/user-operation-service/user/operation/getOperationsForTheLast30DaysByUserEmail?email=${email}`;
     return this.http.get<OperationResponseI[]>(getLast30DaysOperations).pipe(
       map(response => response)
     );
   }
 
   patchPersonalInfoByEmail(form: UserInfoRequestI): Observable<UserInfoResponseI> {
-    let loginEndPoint = `${this.url}/user-registration-service/user/registration/updatePersonalInfoByEmail`;
+    let loginEndPoint = `${this.apiUrl}/user-registration-service/user/registration/updatePersonalInfoByEmail`;
 
     return this.http.patch<UserInfoResponseI>(loginEndPoint, form);
+  }
+
+  refreshToken() {
+    return this.http.post<any>(`${this.apiUrl}/user-auth-service/user/auth/refreshToken`, {
+      'refreshToken': localStorage.getItem('refreshToken')
+    }).pipe(tap((refreshResponse: LoginResponseI) => {
+      this.storeJwtToken(refreshResponse);
+    }));
+  }
+
+  private storeJwtToken(refreshResponse: LoginResponseI) {
+    localStorage.setItem("accessToken", refreshResponse.accessToken);
+    localStorage.setItem("refreshToken", refreshResponse.refreshToken);
   }
 }
